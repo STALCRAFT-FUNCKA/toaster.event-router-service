7e44f407771a5c1e6b409a90179573e27f4f60a0
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-02-27 23:00:07 +1000

    Docs

--- start.py
+++ start.py
@@ -1,4 +1,17 @@
+"""Service "toaster.event-routing-service".
+About:
+    A service that listening to VKontakte longpolling server,
+    receives events from it, and then forms them
+    custom events, sending json data through the client socket
+    about the event to other processing services.
+    
+Author:
+    Oidaho (Ruslan Bashinskii)
+    oidahomain@gmail.com
+"""
+from router import Fetcher
 
+fetcher = Fetcher()
 
 if __name__ == "__main__":
-    pass
+    fetcher.run()
