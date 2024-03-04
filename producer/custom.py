21944c579b593580208b5b05da3e928a36d9065c
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-03-04 12:38:12 +1000

    wokstream-logging-service disabled

--- producer/custom.py
+++ producer/custom.py
@@ -1,3 +1,5 @@
+"""Module "producer".
+"""
 import json
 from .body import Producer
 
@@ -13,26 +15,6 @@ class CustomProducer(Producer):
         "message_new": "messages",
         "button_pressed": "buttons" 
     }
-
-    async def log_workstream(self, logger_name: str, text:str , logging_lvl: str = "info"):
-        """A function that provides the ability to send a log
-        to the general logging service via a queue in RabbitMQ.
-
-        Args:
-            logger_name (str): Name of the logger instance.
-            text (str): Log text.
-            logging_lvl (str, optional): Logging lvl. Defaults to "info".
-        """
-        data = {
-            "name": logger_name,
-            "mode": logging_lvl,
-            "text": text
-        }
-
-        queue = "logs"
-
-        await self._send_data(data, queue)
-
 
     async def transfer_event(self, event: "MessageEvent"):
         """A function that provides the ability to send an event
