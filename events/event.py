6442a6fa28b3566913f18e1f613ec498c438f776
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-06-28 01:20:05 +1000

    reworked event

--- events/event.py
+++ events/event.py
@@ -1,109 +1,18 @@
 """Module "events"."""
 
-from vk_api import VkApi
-import config
-from .base import BaseEvent
 
+class Event(object):
+    """DOCSTRING"""
 
-class MessageEvent(BaseEvent):
-    """Custom message event."""
+    event_id: str = None
+    event_type: str = None
 
-    # user data
-    user_id: int
-    user_name: str
-    user_nick: str
+    def __init__(self, raw_event: dict, event_type: str = "Undefined"):
+        self.event_type = event_type
+        self.event_id = raw_event.get("event_id")
 
-    # peer data
-    peer_id: int
-    peer_name: str
-    chat_id: int
+    def __str__(self):
+        return f"<-- class Event <type: {self.event_type}> | <id: {self.event_id}> -->"
 
-    # message data
-    cmid: int
-    text: str  # TODO: addtext validating (Optional)
-    reply: dict
-    forward: list
-    attachments: list
-
-    def __init__(self, raw_event: dict, api: VkApi):
-        super().__init__(raw_event, api)
-        message = raw_event.get("object").get("message")
-
-        self._get_userdata(message)
-        self._get_peerdata(message)
-        self._get_messagedata(message)
-
-        if self.text.startswith(config.COMMAND_PREFIXES):
-            self.event_type = "command_call"
-
-    def _get_userdata(self, message: dict):
-        self.user_id = message.get("from_id")
-        info = self._get_userinfo(self.user_id)
-        self.user_name = " ".join([info.get("first_name"), info.get("last_name")])
-        self.user_nick = info.get("domain")
-
-    def _get_peerdata(self, message: dict):
-        self.peer_id = message.get("peer_id")
-        info = self._get_peerinfo(self.peer_id)
-        self.peer_name = info.get("title")
-        self.chat_id = self.peer_id - config.VK_GROUP_ID_DELAY
-
-    def _get_messagedata(self, message: dict):
-        self.cmid = message.get("conversation_message_id")
-        self.text = message.get("text")
-        self.reply = message.get("reply_message")
-        self.forward = message.get("fwd_messages")
-        self.attachments = [
-            attachment.get("type") for attachment in message.get("attachments")
-        ]
-        if message.get("geo"):
-            self.attachments.append("geo")
-        if self.reply:
-            self.attachments.append("reply")
-        if self.forward:
-            self.attachments.append("forward")
-
-
-class ButtonEvent(BaseEvent):
-    """Custom button event."""
-
-    # user data
-    user_id: int
-    user_name: str
-    user_nick: str
-
-    # peer data
-    peer_id: int
-    peer_name: str
-    chat_id: int
-
-    # button data
-    cmid: int
-    button_event_id: str
-    payload: dict
-
-    def __init__(self, raw_event: dict, api: VkApi):
-        super().__init__(raw_event, api)
-        self.event_type = "button_pressed"
-        message = raw_event.get("object")
-
-        self._get_userdata(message)
-        self._get_peerdata(message)
-        self._get_buttondata(message)
-
-    def _get_userdata(self, message: dict):
-        self.user_id = message.get("user_id")
-        info = self._get_userinfo(self.user_id)
-        self.user_name = " ".join([info.get("first_name"), info.get("last_name")])
-        self.user_nick = info.get("domain")
-
-    def _get_peerdata(self, message: dict):
-        self.peer_id = message.get("peer_id")
-        info = self._get_peerinfo(self.peer_id)
-        self.peer_name = info.get("title")
-        self.chat_id = self.peer_id - config.VK_GROUP_ID_DELAY
-
-    def _get_buttondata(self, message: dict):
-        self.cmid = message.get("conversation_message_id")
-        self.button_event_id = message.get("event_id")
-        self.payload = message.get("payload")
+    def __repr__(self):
+        return self.__str__()
