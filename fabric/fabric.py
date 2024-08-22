6144b992491411a0784b013abc7cf7612c606fd7
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-23 00:51:48 +1000

    Revert "debug"
    This reverts commit 7b6749a3aff944721d475862e0a1d8066092caeb.

--- fabric/fabric.py
+++ fabric/fabric.py
@@ -182,10 +182,6 @@ class Fabric:
         return paylaod
 
     def _get_message_forward_data(self, forward: List[RawData]) -> List[Payload]:
-        from loguru import logger
-
-        logger.debug(forward)
-
         replies = [
             self._get_message_reply_data(fwd) for fwd in forward if fwd.get("peer_id")
         ]
